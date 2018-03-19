a129666195fc0a009df252c0281aecb8de68455a
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-03-20 00:41:13 +0530

    Fragment Added

--- app/src/main/java/com/jt/javatechnocrat/InquiryFragment.java
+++ app/src/main/java/com/jt/javatechnocrat/InquiryFragment.java
@@ -2,21 +2,22 @@ package com.jt.javatechnocrat;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class InquiryFragment extends Fragment {
-    View mView;
+    private AppCompatActivity main;
+    private View root;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        getActivity().getActionBar().setTitle("Inquiry");
+        main=(AppCompatActivity)getActivity();
+        main.getSupportActionBar().setTitle("Inquiry");
+        root=inflater.inflate(R.layout.fragment_inquiry, container, false);
+
         // Inflate the layout for this fragment
-        mView=inflater.inflate(R.layout.fragment_inquiry, container, false);
-
-        return mView;
+        return root;
     }
-
-
 }
