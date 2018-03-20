ba843abd645970fff42f52e6340aebe31dfbbbf0
rk472 <33664106+rk472@users.noreply.github.com>
2018-03-21 02:05:26 +0530

    Courses completed

--- app/src/main/java/com/jt/javatechnocrat/CoursesFragment.java
+++ app/src/main/java/com/jt/javatechnocrat/CoursesFragment.java
@@ -1,10 +1,12 @@
 package com.jt.javatechnocrat;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.renderscript.Script;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -16,13 +18,25 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+import static android.os.Environment.getExternalStorageDirectory;
 
 
 public class CoursesFragment extends Fragment {
@@ -55,10 +69,35 @@ public class CoursesFragment extends Fragment {
                 viewHolder.syllabus.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        String url = model.getSyllabus();
-                        Intent i = new Intent(Intent.ACTION_VIEW);
-                        i.setData(Uri.parse(url));
-                        startActivity(i);
+                        String url=model.getSyllabus();
+                        final ProgressDialog dialog=new ProgressDialog(main);
+                        StorageReference downloadStore= FirebaseStorage.getInstance().getReferenceFromUrl(url);
+                        File root = getExternalStorageDirectory();
+                        File dir=new File(root.getAbsolutePath(),"/JT");
+                        dir.mkdirs();
+                        final File file = new File(dir,model.getName()+".pdf");
+                        downloadStore.getFile(file).addOnProgressListener(new OnProgressListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                dialog.setTitle("Downloading..");
+                                dialog.setMessage(taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount()*100+"%");
+                                dialog.show();
+                            }
+                        }).addOnCompleteListener(new OnCompleteListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<FileDownloadTask.TaskSnapshot> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(main, "Successfully downloaded to "+file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                                }
+                                dialog.dismiss();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(main, e.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+
                     }
                 });
             }
